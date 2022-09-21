c3603051f622332d466a1515a1abefe5977528af
Pelayo <phuermij@myuax.com>
2022-09-21 16:56:43 +0200

    metodo de cuadrante

--- Clases/Conceptos.py
+++ Clases/Conceptos.py
@@ -5,3 +5,28 @@ class Punto():
         self.x = x
         self.y = y
     
+    def cuadrante(self):
+        if self.x > 0 and self.y > 0:
+            print("Estás en el primer cuadrante.")
+        
+        elif self.x < 0 and self.y > 0:
+            print("Estás en el segundo cuadrante")
+
+        elif self.x < 0 and self.y < 0:
+            print("Estás en el tercer cuadrante")
+
+        elif self.x >0 and self.y < 0:
+            print("Estás en el cuarto cuadrante")
+        
+        elif self.x == 0 and self.y != 0:
+            print("Estás en el eje horizontal")
+        
+        elif self.x != 0 and self.y == 0:
+            print("Estás en el eje vertical")
+        
+        else:
+            print("Estás en el origen de coordenadas")
+    
+        
+
+    
