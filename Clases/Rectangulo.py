c352b15c54281a7e88a042aebe29d5fc7dbb1e84
Pelayo <phuermij@myuax.com>
2022-09-21 17:30:40 +0200

    getters y setters

--- Clases/Rectangulo.py
+++ Clases/Rectangulo.py
@@ -5,5 +5,22 @@ class Rectangulo():
         self.pto1 = pto1
         self.pto2 = pto2
 
-    
+    #Getters
+    def get_pto1(self):
+        return self.pto1
 
+    def get_pto2(self):
+        return self.pto2
+
+    #Setters
+    def set_pto1(self, pto1):
+        if pto1 == "":
+            pto1 = (0,0)
+        else:
+            self.pto1 = pto1
+
+    def set_pto1(self, pto2):
+        if pto2 == "":
+            pto2 = (0,0)
+        else:
+            self.pto2 = pto2
